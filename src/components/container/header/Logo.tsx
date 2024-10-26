a6a1964d3d928099029aec045e97beb1858a6967
Anmol Shah <anmolsha80@gmail.com>
2024-10-26 22:32:12 +0545

    features/add-the-feedback-to-feedback-list - Add a blank line at the end of each of these components i.e., Logo, PageHeading and Pattern

--- src/components/container/header/Logo.tsx
+++ src/components/container/header/Logo.tsx
@@ -2,10 +2,10 @@ import logo from '@/assets/logo.svg';
 
 const Logo = () => {
   return (
-    <a href='/' className='logo'>
+    <a href="/" className="logo">
       <img src={logo} alt="logo" />
     </a>
-  )
-}
+  );
+};
 
 export default Logo;
--- src/components/container/header/Pattern.tsx
+++ src/components/container/header/Pattern.tsx
@@ -1,9 +1,7 @@
-import patternImg from '@/assets/pattern.svg'
+import patternImg from '@/assets/pattern.svg';
 
 const Pattern = () => {
-  return (
-    <img src={patternImg} alt="pattern" className='pattern' />
-  )
-}
+  return <img src={patternImg} alt="pattern" className="pattern" />;
+};
 
 export default Pattern;
