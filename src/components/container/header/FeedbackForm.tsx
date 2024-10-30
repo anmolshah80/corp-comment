37dce6f9cacefee5f9fc7eafc1b3fc40dd0921ef
Anmol Shah <anmolsha80@gmail.com>
2024-10-30 23:29:06 +0545

    features/upvote-or-downvote-feedbacks-on-click - Upvote or downvote feedbacks upon clicking the button - Focus textarea field on forward slash keydown

--- src/components/container/header/FeedbackForm.tsx
+++ src/components/container/header/FeedbackForm.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import { MAX_CHARACTERS } from '@/lib/constants';
 
@@ -8,6 +8,11 @@ type FeedbackFormProps = {
 
 const FeedbackForm = ({ handleAddToList }: FeedbackFormProps) => {
   const [text, setText] = useState('');
+  const [showValidIndicator, setShowValidIndicator] = useState(false);
+  const [showInvalidIndicator, setShowInvalidIndicator] = useState(false);
+
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+  const slashKeyCounterRef = useRef(0);
 
   const remainingCharacters = MAX_CHARACTERS - text.length;
 
@@ -24,32 +29,86 @@ const FeedbackForm = ({ handleAddToList }: FeedbackFormProps) => {
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
+    if (text.includes('#') && text.length > 5) {
+      setShowValidIndicator(true);
+
+      setTimeout(() => setShowValidIndicator(false), 2000);
+    } else {
+      setShowInvalidIndicator(true);
+
+      setTimeout(() => setShowInvalidIndicator(false), 4000);
+
+      return;
+    }
+
     handleAddToList(text);
 
     setText('');
+    setShowInvalidIndicator(false);
   };
 
+  const formClassName = `form ${showValidIndicator ? 'form--valid' : ''} ${
+    showInvalidIndicator ? 'form--invalid' : ''
+  }`;
+
+  useEffect(() => {
+    const focusTextareaWithHotKey = (event: KeyboardEvent) => {
+      if (event.key !== '/') return;
+
+      if (textareaRef.current === null) return;
+
+      // prevent adding the slash key in the textarea upon entering the slash key for the first time
+      if (slashKeyCounterRef.current === 0) {
+        event.preventDefault();
+
+        textareaRef.current.focus();
+        slashKeyCounterRef.current += 1;
+
+        return;
+      }
+
+      textareaRef.current.focus();
+    };
+
+    document.addEventListener('keydown', focusTextareaWithHotKey);
+
+    return () => {
+      document.removeEventListener('keydown', focusTextareaWithHotKey);
+    };
+  }, []);
+
   return (
-    <form className="form" onSubmit={handleSubmit}>
-      <textarea
-        id="feedback-textarea"
-        value={text}
-        placeholder=""
-        spellCheck={false}
-        onChange={handleChange}
-      />
+    <>
+      <form className={formClassName} onSubmit={handleSubmit}>
+        <textarea
+          id="feedback-textarea"
+          value={text}
+          placeholder=""
+          spellCheck={false}
+          onChange={handleChange}
+          ref={textareaRef}
+          onBlur={() => (slashKeyCounterRef.current = 0)}
+        />
 
-      <label htmlFor="feedback-textarea">
-        Enter your feedback here, remember to #hashtag the company
-      </label>
+        <label htmlFor="feedback-textarea">
+          Enter your feedback here, remember to #hashtag the company. Enter{' '}
+          <span className="slash-key">/</span> to start typing.
+        </label>
 
-      <div>
-        <p className="u-italic">{remainingCharacters} characters remaining</p>
-        <button>
-          <span>Submit</span>
-        </button>
-      </div>
-    </form>
+        <div>
+          <p className="u-italic">{remainingCharacters} characters remaining</p>
+          <button>
+            <span>Submit</span>
+          </button>
+        </div>
+      </form>
+      {showInvalidIndicator && (
+        <p className="error-message--form">
+          Either a hashtag is missing or the feedback is less than 5 characters
+          long
+        </p>
+      )}
+    </>
   );
 };
 
